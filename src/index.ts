326ceefa2e0b3f0dd614d34f1c7f9b1b0a63a8d0
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-06 18:58:55 -0300

    first-commit

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+
+const app = express();
+const PORT = (process.env.PORT || 3000);
+
+app.use(express.json());
+app.listen(PORT, () => {
+    console.log(`Rodando em http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    return res.status(200)
+    .send('<h1 style="text-align: center; margin-top: 50px;">App rodando</h1>');
+});
+
+let pessoas = [
+    {"id": "0", "nome": "joao0"},
+];
+
+app.get('/pessoas', (req, res) => {
+    return res.status(200).send(pessoas);
+});
+
+app.get('/pessoas/:id', (req, res) => {
+    const pessoaId = req.params.id;
+    const pessoa = pessoas.find(pessoa => {
+        return (pessoa.id === pessoaId);
+    });
+    return res.status(200).send(pessoa);
+});
+
+app.post('/pessoas', (req, res) => {
+    const pessoa = req.body;
+    pessoas.push(pessoa);
+    return res.status(201).send(pessoas);
+});
+
+app.put('/pessoas/:id', (req, res) => {
+    const pessoaId = req.params.id;
+    const pessoaAtt = req.body;
+
+    pessoas = pessoas.map(pessoa => {
+        if (pessoaId === pessoa.id) {
+            pessoa.nome = pessoaAtt.nome;
+        }
+        return pessoa;
+    });
+    return res.status(200).send(pessoaAtt);
+});
+
+app.delete('/pessoas/:id', (req, res) => {
+    const pessoaId = req.params.id;
+    pessoas = pessoas.filter((pessoa) => pessoa.id !== pessoaId);
+    return res.status(204).send({message: "Usuário excluído"});
+});
